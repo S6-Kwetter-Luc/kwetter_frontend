6e810368a368f6667016156799c791323823f9c0
Luc Urlings <urlingsluc@gmail.com>
2020-04-08 14:07:49 +0200

    KL-37: Added github actions

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\. Programma's\Flutter_SDK\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\lsrur\Documents\GitHub\Fontys-semester-6\Individueel-project\kwetter_frontend"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\. Programma's\Flutter_SDK\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
